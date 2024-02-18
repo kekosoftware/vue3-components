f103e4c2f10a7cee33584ad141cc43bf31fa873f
Gustavo Ariel Coronel <ariel256@gmail.com>
2024-02-18 09:28:56 -0300

    first commit of this tutorial

---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+    <grid>
+        <img :src="url" :alt="nombre">
+        <h2>{{ nombre }}</h2>
+
+        <a :href="url" target="_blank">
+            Descargar
+        </a>
+    </grid>
+</template>
+
+<script>
+import Grid from "./Grid.vue"
+
+export default {
+    props: {
+        url: {
+            String
+        },
+        nombre: {
+            String
+        }
+    },
+    components: { Grid }
+}
+</script>
+
+<style scoped>
+
+img {
+    width: 15rem;
+    height: 15rem;
+    background: snow;
+    padding: 1rem;
+}
+
+a {
+    text-decoration: none;
+    color: snow;
+    background-color: #301675;
+    padding: 0.5rem;
+    border-radius: 5px;
+    border: 1px solid #f2f2f2;
+}
+
+</style>
